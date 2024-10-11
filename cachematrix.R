cedd28cd90c26051a0fa6f2f52f0d04f2183db2c
Redowan <redowansakib01@gmail.com>
2024-10-12 03:36:37 +0600

    Assignment Done!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
+## set the environment 
+
+install.packages("matlib")
+library(matlib)
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function takes a matrix and gives it a structure where its inverse can
+## be cached
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv.mat <- NULL
+  set <- function(y){
+    x <<- y
+    inv.mat <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse_matrix) inv.mat <<- inverse_matrix
+  getinv <- function() inv.mat
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This Function takes a matrix and returns its inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv.mat <- x$getinv()
+  if (!is.null(inv.mat)){
+    message("getting cached data")
+    return(inv.mat)
+  }
+  data <- x$get()
+  inv.mat <- inv(data, ...)
+  x$setinv(inv.mat)
+  inv.mat
 }
